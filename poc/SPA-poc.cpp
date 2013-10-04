9ab0a2b13213b2f49b3d3b86600607a18d9e440f
Lukas Hellebrandt <kamikazecz@gmail.com>
2013-10-04 03:14:32 +0200

    i = i++ pattern detection

--- poc/SPA-poc.cpp
+++ poc/SPA-poc.cpp
@@ -13,21 +13,51 @@ namespace {
 
 class SPA_pocVisitor
   : public RecursiveASTVisitor<SPA_pocVisitor> {
+private:
+  bool side_effect_race(Stmt *S){
+	if(S->child_begin()->getStmtClass()!=60){
+		return false;
+	}
+	NamedDecl *target_addr = static_cast<clang::DeclRefExpr*>(*(S->child_begin()))->getFoundDecl();
+	if((++(S->child_begin()))->getStmtClass()!=109){
+		return false;
+	}
+	UnaryOperator *inc = static_cast<clang::UnaryOperator*>(*(++(S->child_begin())));
+	if(inc->child_begin()->getStmtClass()!=60){
+		return false;
+	}
+	NamedDecl *origin_addr = static_cast<clang::DeclRefExpr*>(*(inc->child_begin()))->getFoundDecl();
+  	return target_addr==origin_addr;
+  }
+
 public:
   bool VisitStmt(Stmt *S){
-    std::cout << S->getStmtClass() << " -- ";
-    std::cout << S->getStmtClassName();
-    if(S->getStmtClass()==60){
-        std::cout << " <" << (static_cast<clang::DeclRefExpr*>(S))->getFoundDecl()->getQualifiedNameAsString() << ">[" << (static_cast<clang::DeclRefExpr*>(S))->getFoundDecl() << "]";
+    /*std::cout << S->getStmtClass() << " -- " << S->getStmtClassName();
+    if(S->getStmtClass()==60){//DeclRefExpr
+	NamedDecl * ND = (static_cast<clang::DeclRefExpr*>(S))->getFoundDecl();
+        std::cout << " <" << ND->getQualifiedNameAsString() << ">[" << ND << "]";
+    }*/
+    if(S->getStmtClass()==20){//BinaryOperator
+        BinaryOperator *BO = static_cast<BinaryOperator *>(S);
+	if(BO->isAssignmentOp()){
+	    /*std::cout << " {Assignment}";
+    	    std::cout << std::endl;*/
+	    if(std::distance(BO->child_begin(),BO->child_end())==2 && side_effect_race(S)){
+	    	std::cout << "Warning: side effect race [-plugin SPA-poc]" << std::endl;
+	    }
+            //std::cout << " // " << std::distance(BO->child_begin(),BO->child_end()) << " " << BO->child_begin()->getStmtClass();
+	}
     }
-    std::cout << std::endl;
+    /*else{
+        std::cout << std::endl;
+    }*/
     return true;
   }
 
   bool VisitNamedDecl(NamedDecl *D) {
     // For debugging, dumping the AST nodes will show which nodes are already
     // being visited.
-    std::cout << D->getKind() << " -- " << D->getDeclKindName() << " -- " << D->getNameAsString() << std::endl;
+    //std::cout << D->getKind() << " -- " << D->getDeclKindName() << " -- " << D->getNameAsString() << std::endl;
 
     // The return value indicates whether we want the visitation to proceed.
     // Return false to stop the traversal of the AST.
@@ -39,7 +69,7 @@ class SPA_pocConsumer : public clang::ASTConsumer {
 public:
   virtual void HandleTranslationUnit(clang::ASTContext &Context) {
     // Traversing the translation unit decl via a RecursiveASTVisitor
-    // will visit all nodes in the AST.
+    // will visit all nodes in the AST.clang -Xclang -load -Xclang lib/SPA-poc.so -Xclang -add-plugin -Xclang SPA-poc ~/Desktop/bc/test.c -o test; echo "-----"; ./test; result=$?; echo "-----"; echo $result;
     Visitor.TraverseDecl(Context.getTranslationUnitDecl());
   }
 private:
